1740f3aedb673416cd901374a275bd3284f2ba59
AsrorWali <sardor-g@hotmail.com>
2017-03-22 09:12:38 +0100

    Test
    Jenkins build test

--- Kwetter2/Kwetter-web/src/main/test/impl/UserServiceImplTest.java
+++ Kwetter2/Kwetter-web/src/main/test/impl/UserServiceImplTest.java
@@ -15,47 +15,15 @@ import static org.junit.Assert.*;
 /**
  * Created by Asror on 12-3-2017.
  */
-@RunWith(MockitoJUnitRunner.class)
 public class UserServiceImplTest {
 
-    private UserServiceImpl service;
-    @Mock
-    private UserDAO ud;
-
-    public UserServiceImplTest() {
-    }
-
-    @Before
-    public void setUp() {
-        service = new UserServiceImpl();
-        service.setUd(ud);
-    }
 
     @Test
     public void getUser() throws Exception {
-        User t = service.getUser(1L);
-        Mockito.verify(ud).find(1L);
+        int a = 1;
+        int b = 2;
+        assertEquals(a,b);
     }
 
-    @Test
-    public void getUser2() throws Exception {
-        User t = service.getUser(2L);
-        assertEquals(t, null);
-    }
-
-    @Test
-    public void addUser() throws Exception {
-        User u = new User("Asror", "", "Asror", "Hallo, ik ben Asror", "Rijen", "walicorp,com");
-        service.addUser(u);
-        Mockito.verify(ud).create(u);
-    }
-
-    @Test
-    public void addUser2() throws Exception {
-        User u = new User("Asror", "", "Asror", "Hallo, ik ben Asror", "Rijen", "walicorp,com");
-        User u2 = new User("Asror", "", "Asror", "Hallo, ik ben Asror", "Rijen", "walicorp,com");
-        service.addUser(u);
-        Mockito.verify(ud).create(u2);
-    }
 
 }
