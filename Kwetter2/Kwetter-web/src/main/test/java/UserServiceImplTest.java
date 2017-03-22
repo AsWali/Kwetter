4a2b29ab5c76f8fec0a1e311e7d0ea93d1e5e0b1
AsrorWali <sardor-g@hotmail.com>
2017-03-22 11:08:23 +0100

    different folder
    lets go

--- Kwetter2/Kwetter-web/src/main/test/java/UserServiceImplTest.java
+++ Kwetter2/Kwetter-web/src/main/test/java/UserServiceImplTest.java
@@ -1,22 +1,12 @@
-package impl;//package service.impl;
+package java;
 
-import dao.inter.UserDAO;
-import domain.User;
-import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.mockito.Mock;
-import org.mockito.Mockito;
-import org.mockito.runners.MockitoJUnitRunner;
-import service.impl.UserServiceImpl;
-
 import static org.junit.Assert.*;
 
 /**
  * Created by Asror on 12-3-2017.
  */
 public class UserServiceImplTest {
-
 
     @Test
     public void getUser() throws Exception {
@@ -31,6 +21,5 @@ public class UserServiceImplTest {
         int b = 1;
         assertEquals(a,b);
     }
-
 
 }
